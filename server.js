cb0e57f2f55625545763c208bbacfdd2f2bbf889
Rudrakumar Patel <rudrapatel2992003@gmail.com>
2025-02-02 22:21:01 +0530

    update

--- server.js
+++ server.js
@@ -1,29 +1,3 @@
-// import express from "express";
-// import dotenv from "dotenv";
-// import cors from "cors";
-// import connectDB from "./config/db.js";
-// import authRoutes from "./routes/authRoutes.js";
-// import noteRoutes from "./routes/noteRoutes.js";
-// import path from "path";
-
-// dotenv.config();
-// const app = express();
-// connectDB();
-
-// app.use(cors());
-// app.use(express.json());
-// app.use("/api/auth", authRoutes);
-// app.use("/api/notes", noteRoutes);
-
-// app.get("/",(req, res) => {
-//   app.use(express.static(path.resolve(__dirname, "frontend","dist")));
-//   res.sendFile(path.resolve(__dirname, "frontend","dist","index.html"));
-// })
-
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
@@ -50,10 +24,10 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 
-app.use(express.static(path.resolve(__dirname, "frontend", "dist")));
+app.use(express.static(path.resolve(__dirname, "frontend", "build")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
 });
 
 // Start Server
