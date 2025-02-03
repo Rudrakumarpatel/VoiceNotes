489a33a358f9c5be7d212bed40dc7df35bdc0fff
Rudrakumar Patel <133936044+Rudrakumarpatel@users.noreply.github.com>
2025-02-03 05:14:47 +0000

    update

--- server.js
+++ server.js
@@ -5,12 +5,11 @@ import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 import noteRoutes from "./routes/noteRoutes.js";
 import path from "path";
-import { fileURLToPath } from "url";  
+import { fileURLToPath } from "url";
 
 dotenv.config();
 const app = express();
 connectDB();
-
 
 app.use(cors());
 app.use(express.json());
@@ -19,10 +18,8 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/notes", noteRoutes);
 
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
 
 app.use(express.static(path.resolve(__dirname, "frontend", "build")));
 
